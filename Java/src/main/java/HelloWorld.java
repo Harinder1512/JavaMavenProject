8ebd993ac81051d562fa42e7399916a32d61ff8e
Harinder <harinderreddy1512@gmail.com>
2023-11-28 18:33:06 +0530

    from git repo window ..

--- Java/src/main/java/HelloWorld.java
+++ Java/src/main/java/HelloWorld.java
@@ -4,6 +4,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("from local repository");
-		System.out.println("from remote repository");
 	} 
 }
