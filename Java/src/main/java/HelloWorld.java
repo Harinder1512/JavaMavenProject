abb8d763aa315f2e0a1f3b9e548559c755eeeea7
Harinder <harinderreddy1512@gmail.com>
2023-11-29 11:17:00 +0530

    sop statement modified ..

--- Java/src/main/java/HelloWorld.java
+++ Java/src/main/java/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("from local repository");
-		System.out.println("from Remote repository");
+		System.out.println("from Remote repository "+44);
 	} 
 }
