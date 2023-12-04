9d6a015a93c61e7167ac014120d4e72f86297b93
Harinder Reddy K <harinderreddykk@gmail.com>
2023-12-04 12:09:17 +0530

    new changes 4-12-23

--- Java/src/main/java/HelloWorld.java
+++ Java/src/main/java/HelloWorld.java
@@ -2,8 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		System.out.println("from local repository");
-		System.out.println("from Remote repository "+44);
+		System.out.println("from Remote repository ");
 	} 
 }
